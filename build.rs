06917d573561c6459fa6b4d805196bfe2c83bf95
dimi309 <dimi309@users.noreply.github.com>
2023-03-07 22:31:15 +0100

    Successfully calling vulkan_helper functions.

--- build.rs
+++ build.rs
@@ -4,15 +4,20 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+
+    let vulkan_sdk = env::var("VULKAN_SDK").expect("vulkan sdk environment variable not found");
+
     println!("cargo:rustc-link-search=lib");
+    println!("cargo:rustc-link-search={}{}", vulkan_sdk, r#"\Lib"#);
 
     println!("cargo:rustc-link-lib=vulkan_helper");
+    println!("cargo:rustc-link-lib=vulkan-1");
 
     println!("cargo:rerun-if-changed=wrapper.h");
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg("-I".to_owned() + &env::var("VULKAN_SDK").expect("vulkan sdk environment variable not found") + r#"\Include"#)
+        .clang_arg("-I".to_owned() + &vulkan_sdk + r#"\Include"#)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
@@ -21,6 +26,5 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
-
     
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
 
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+use std::ffi::CString;
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
 fn main() {
 
-    println!("Hello!");
+    // Binding the CString to a variable so that it does not get deallocated
+    
+    let nameStr = CString::new("Hello Rust").expect("CString::new failed");
+    let namep = nameStr.as_ptr();
+
+    const exts: *mut *const std::os::raw::c_char = std::ptr::null_mut();
+
+    unsafe {
+
+        // Using the vulkan helper
+        let ret = vh_create_instance(namep, exts, 0);
+        if ret > 0 {
+            println!("Vulkan worked!")
+        }
+        vh_shutdown();
+    }
+
 }
