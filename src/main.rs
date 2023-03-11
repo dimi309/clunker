cd49134d4f11f68c139e017bb58d41c53b722744
dimi309 <dimi309@users.noreply.github.com>
2023-03-11 19:09:39 +0100

    Passing an extension without error to the vulkan helper.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@
 #![allow(unused_assignments)]
 
 use std::ffi::CString;
+use std::os::raw::c_char;
+const ARRAY_SIZE: usize = 30;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
@@ -15,17 +17,30 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
-
-static binding_desc: VkVertexInputBindingDescription = VkVertexInputBindingDescription { binding: 0, stride: 0, inputRate: 0 };
-
-unsafe extern "C" fn set_input_state_callback(insputStateCreateInfo: *mut VkPipelineVertexInputStateCreateInfo) -> i32 {
+unsafe extern "C" fn set_input_state_callback(
+    insputStateCreateInfo: *mut VkPipelineVertexInputStateCreateInfo,
+) -> i32 {
     println!("Input state callback called.");
     1
-} 
+}
 
-unsafe extern "C" fn set_pipeline_layout_callback(pipelineLayoutCreateInfo: *mut VkPipelineLayoutCreateInfo) -> i32 {
+unsafe extern "C" fn set_pipeline_layout_callback(
+    pipelineLayoutCreateInfo: *mut VkPipelineLayoutCreateInfo,
+) -> i32 {
     println!("Pipeline layout callback called.");
     1
+}
+
+unsafe fn to_c_str(byte_sequence: &[u8], array: &mut [i8; ARRAY_SIZE])  {
+    let mut index = 0;
+    while index < byte_sequence.len() && index + 1 < ARRAY_SIZE {
+        if byte_sequence[index] != 0 {
+            array[index] = byte_sequence[index] as c_char;
+            index += 1;
+        } else {
+            break;
+        }
+    }
 }
 
 fn main() {
@@ -68,24 +83,57 @@ struct App {
 
 impl App {
     unsafe fn create(window: &Window) -> App {
-        let myself = Self {nameStr: CString::new("Hello Rust").expect("CString::new failed"), pipeline_index: &mut 100};
-        let vertex_sharder_path = CString::new(std::env::current_dir().unwrap().to_str().unwrap().to_string() + 
-        "\\resources\\shaders\\vertexShader.spv").expect("CString::new failed");
-        let fragment_shader_path = CString::new(std::env::current_dir().unwrap().to_str().unwrap().to_string() + 
-        "resources\\shaders\\vertexShader.spv").expect("CString::new failed");
+        let myself = Self {
+            nameStr: CString::new("Hello Rust").expect("CString::new failed"),
+            pipeline_index: &mut 100,
+        };
+        let vertex_sharder_path = CString::new(
+            std::env::current_dir()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string()
+                + "\\resources\\shaders\\vertexShader.spv",
+        )
+        .expect("CString::new failed");
+        let fragment_shader_path = CString::new(
+            std::env::current_dir()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string()
+                + "resources\\shaders\\vertexShader.spv",
+        )
+        .expect("CString::new failed");
         let mut res: i32 = 0;
 
+       
+
+        let mut array = [0i8; ARRAY_SIZE];
+
+        to_c_str(VK_KHR_SURFACE_EXTENSION_NAME, &mut array);
+
+        let b = [array.as_ptr()].as_mut_ptr();
+
         // Using the vulkan helper
-        res = vh_create_instance(myself.nameStr.as_ptr(), std::ptr::null_mut(), 0);
+        res = vh_create_instance(myself.nameStr.as_ptr(), b, 1);
 
-        let iscb = 
-        Option::Some(set_input_state_callback as unsafe extern "C" fn(*mut VkPipelineVertexInputStateCreateInfo) -> i32);
-        let iscc = 
-        Option::Some(set_pipeline_layout_callback as unsafe extern "C" fn(*mut VkPipelineLayoutCreateInfo) -> i32);
+        let iscb = Option::Some(
+            set_input_state_callback
+                as unsafe extern "C" fn(*mut VkPipelineVertexInputStateCreateInfo) -> i32,
+        );
+        let iscc = Option::Some(
+            set_pipeline_layout_callback
+                as unsafe extern "C" fn(*mut VkPipelineLayoutCreateInfo) -> i32,
+        );
 
-        vh_create_pipeline(vertex_sharder_path.as_ptr(), fragment_shader_path.as_ptr(), 
-            iscb, iscc, myself.pipeline_index);
-        
+        vh_create_pipeline(
+            vertex_sharder_path.as_ptr(),
+            fragment_shader_path.as_ptr(),
+            iscb,
+            iscc,
+            myself.pipeline_index,
+        );
 
         if res > 0 {
             println!("Vulkan worked!")
