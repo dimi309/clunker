48c1e325f15a048101b61191738ba34fbdce11c9
dimi309 <dimi309@users.noreply.github.com>
2024-08-14 06:42:21 +0200

    Refactored model a bit.

--- src/model.rs
+++ src/model.rs
@@ -1,61 +1,76 @@
 #[derive(Default)]
 pub struct Model {
+    pub buffers: Vec<gltf::buffer::Data>,
     pub vertexData: Vec<f32>,
     pub indexData: Vec<u16>,
 }
 
 impl Model {
-    pub fn load(&mut self, filepath: &str) {
-        let (document, buffers, _) =
-            gltf::import(filepath).expect("Error while importing document, buffers and images");
+    fn readVertexData(&mut self, primitives: &Vec<gltf::Primitive>) {
+        let pos = primitives[0]
+            .get(&gltf::Semantic::Positions)
+            .expect("no positions");
+        assert!(5126 == pos.data_type().as_gl_enum()); // float (4 bytes)
+        let posView = pos.view().expect("Could not find positions view");
 
-        if document.meshes().count() > 0 {
-            let a = document.meshes().nth(0).expect("Could not retrieve mesh.");
-            let b: Vec<gltf::Primitive> = a.primitives().collect();
+        let vertexSlice = &self.buffers[posView.buffer().index()]
+            [posView.offset()..posView.offset() + posView.length()];
 
-            if b.len() > 0 {
-                let pos = b[0].get(&gltf::Semantic::Positions).expect("no positions");
-                assert!(5126 == pos.data_type().as_gl_enum()); // float (4 bytes)
-                let posView = pos.view().expect("Could not find positions view");
+        let vertexDataTmp: Vec<f32> = vertexSlice
+            .chunks_exact(4)
+            .map(TryInto::try_into)
+            .map(Result::unwrap)
+            .map(f32::from_le_bytes)
+            .collect();
 
-                let vertexSlice = &buffers[posView.buffer().index()]
-                    [posView.offset()..posView.offset() + posView.length()];
+        let mut counter = 0;
 
-                let vertexDataTmp: Vec<f32> = vertexSlice
-                    .chunks_exact(4)
-                    .map(TryInto::try_into)
-                    .map(Result::unwrap)
-                    .map(f32::from_le_bytes)
-                    .collect();
-
-                let mut counter = 0;
-
-                for vt in vertexDataTmp {
-                    counter = counter + 1;
-                    if counter == 3 {
-                        self.vertexData.push(vt + 0.5);
-                        self.vertexData.push(1f32);
-                        counter = 0;
-                    } else {
-                        self.vertexData.push(vt / 2.0);
-                    }
-                }
-
-                let ind = &b[0].indices().expect("No indices index found");
-                assert!(5123 == ind.data_type().as_gl_enum()); // unsigned short (2)
-
-                let indexView = ind.view().expect("View not found");
-
-                let indexSlice = &buffers[indexView.buffer().index()]
-                    [indexView.offset()..indexView.offset() + indexView.length()];
-
-                self.indexData = indexSlice
-                    .chunks_exact(2)
-                    .map(TryInto::try_into)
-                    .map(Result::unwrap)
-                    .map(u16::from_le_bytes)
-                    .collect();
+        for vt in vertexDataTmp {
+            counter = counter + 1;
+            if counter == 3 {
+                self.vertexData.push(vt + 0.5);
+                self.vertexData.push(1f32);
+                counter = 0;
+            } else {
+                self.vertexData.push(vt / 2.0);
             }
         }
+    }
+
+    fn readIndexData(&mut self, primitives: &Vec<gltf::Primitive>) {
+        let ind = &primitives[0].indices().expect("No indices index found");
+        assert!(5123 == ind.data_type().as_gl_enum()); // unsigned short (2)
+
+        let indexView = ind.view().expect("View not found");
+
+        let indexSlice = &self.buffers[indexView.buffer().index()]
+            [indexView.offset()..indexView.offset() + indexView.length()];
+
+        self.indexData = indexSlice
+            .chunks_exact(2)
+            .map(TryInto::try_into)
+            .map(Result::unwrap)
+            .map(u16::from_le_bytes)
+            .collect();
+    }
+
+    pub fn load(&mut self, filepath: &str) {
+        let (document, buffers1, _) =
+            gltf::import(filepath).expect("Error while importing document, buffers and images");
+
+        self.buffers = buffers1;
+
+        if document.meshes().count() == 0 {
+            return;
+        }
+        let mesh = document.meshes().nth(0).expect("Could not retrieve mesh.");
+        let primitives: Vec<gltf::Primitive> = mesh.primitives().collect();
+
+        if primitives.len() == 0 {
+            return;
+        }
+        
+        self.readVertexData(&primitives);
+        self.readIndexData(&primitives);
     }
 }
